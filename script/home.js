159850443b088b779266ea07e42537d495e736c9
Lucky2k21 <lakshmi.pg@outlook.com>
2022-11-26 16:31:56 -0800

    location

--- script/home.js
+++ script/home.js
@@ -11,3 +11,34 @@ let popupCloseButton=document.querySelector('#couponClose')
 popupCloseButton.addEventListener('click',()=>{
     document.querySelector('#coupon').style.display='none';
 })
+
+//location & weather//
+let x = document.getElementById('out');
+            let y = document.getElementById('weather');
+
+            function geolocation(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(showPosition)
+                }else{
+                    x.innerText = "Geo Not Supported"
+                }
+            }
+
+            function showPosition(data){
+                console.log(data);
+                let lat = data.coords.latitude;
+                let long = data.coords.longitude;
+
+                const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+                ///api calling
+                fetch(url,{method: 'GET'})
+                //return promise
+                .then((res) => res.json())
+                //return data
+                .then((data) => {
+                    console.log(data)
+                    let city = data.city.name
+                    let temp = data.list[0].temp.day
+                    y.innerText = `  ${city} /  ${temp} °C`
+                })
+            }
